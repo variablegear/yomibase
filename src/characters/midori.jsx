fbe71be40efa24a8d77ef4b2ded221c6620aabe5
hobusu <the.isaac.adams@gmail.com>
2017-05-15 12:20:09 -0400

    Added 1st Ed. Midori as a variant

--- src/characters/midori.jsx
+++ src/characters/midori.jsx
@@ -46,10 +46,13 @@ export const midori = {
         innateAbilities: [
             {
                 name: 'Aspect of the Dragon',
-                text: 'Your Dragon attacks can not be dodged by normal dodges. Whenever you block \
-                       an attack or Joker while in Dragon Form, you may return a non-Joker card \
-                       from your discard pile to your hand instead of drawing a card. (You can \
-                       only play Dragon moves while in Dragon Form.)',
+                text: <span>
+                    Your Dragon attacks can not be dodged by normal dodges.
+                    Whenever you block an attack or Joker while in Dragon Form,
+                    you may return a non-Joker card from your discard pile to
+                    your hand instead of drawing a card.<br />
+                    <i>(You can only play Dragon moves while in Dragon Form.)</i>
+                </span>
             },
             {
                 name: 'Defense Mastery',
@@ -68,8 +71,11 @@ export const midori = {
                 rank: 'T',
                 name: 'Glimpse of the Dragon',
                 timing: 'Combat Reveal',
-                text: 'If you combat-revealed a face card while not in Dragon Form, rotate it 180 \
-                       degrees to play the Dragon Form version of your move. (Aces aren\'t face cards.)',
+                text: <span>
+                    If you combat-revealed a face card while not in Dragon Form,
+                    rotate it 180 degrees to play the Dragon Form version of your
+                    move. <i>(Aces aren't face cards.)</i>
+                </span>
             },
         ],
     },
@@ -124,8 +130,8 @@ midori.variants = {
                 {
                     name: 'Essense of the Dragon',
                     text: <span>
-                        Your Dragon attacks can not be dodged.
-                        (You can only play Dragon moves while in Dragon Form.)
+                        Your Dragon attacks can not be dodged.<br />
+                        <i>(You can only play Dragon moves while in Dragon Form.)</i><br />
                         Whenever you block an attack or Joker, you may return a non-
                         Joker card from your discard pile (in addition to drawing from
                         your block).
@@ -151,5 +157,64 @@ midori.variants = {
             ]),
             dodges: midori.summary.dodges.concat(['D']),
         }),
+    }),
+    
+    FirstEd: Object.assign({}, midori, {
+        summary: Object.assign({}, midori.summary, {
+            name: <span>1<sup>st</sup> Ed. {midori.summary.name}</span>,
+            fullName: <span>1<sup>st</sup> Edition {midori.summary.fullName}</span>,
+            innateAbilities: [
+                {
+                    name: 'Aspect of the Dragon',
+                    text: <span>
+                        Your Dragon attacks can not be dodged. This ability can not be countered.<br />
+                        <i>(You can only play Dragon moves while in Dragon Form.)</i>
+                    </span>
+                },
+            ],
+            cardAbilities: [
+                {
+                    rank: 2,
+                    name: 'Dragon Form',
+                    timing: null,
+                    text: <span>
+                        After the draw phase, you may play this card face up on the table to enter
+                        Dragon Form (so that you can play Dragon abilities). Put this card in the
+                        discard pile if you are thrown or if you reveal a non-Dragon attack or
+                        non-Dragon throw in combat.
+                    </span>
+                },
+                {
+                    rank: 'T',
+                    name: 'Glimpse of the Dragon',
+                    timing: null,
+                    text: <span>
+                        After combat cards are revealed, if you played a face card (not an Ace)
+                        while not in Dragon Form, then you may discard this card. Rotate your
+                        combat card 180 degrees to play the Dragon Form version of your
+                        attack or throw.
+                    </span>
+                },
+            ],
+        }),
+        throws: [
+            normalThrow(2),
+            normalThrow(3),
+            normalThrow(4),
+            normalThrow(5),
+            normalThrow(9),
+            normalThrow('T'),
+            {speed: 4.4, rank: 'K', name: 'Rushing River', damage: 12, comboType: <CantCombo/>, comboPts: null},
+            {
+                speed: 2.2, rank: 'K', rankDisplay: <Dragon>K</Dragon>, name: <Dragon>Talon Swoop</Dragon>, damage: 17, comboType: <CantCombo/>,
+                comboPts: null, notes: dragon,
+            },
+            {
+                speed: 0.0, rank: 'AA', rankDisplay: <Dragon>AA</Dragon>, pumpWith: '+A+A',
+                name: <Dragon>Final Dragon Buster</Dragon>, damage: 20,
+                pump: 16, comboType: <CantCombo/>, comboPts: null,
+                maxCombo: 'tAA++', maxDamage: 52, goodCombo: 'tAA+', goodDamage: 36, notes: dragon,
+            },
+        ],
     }),
 };
