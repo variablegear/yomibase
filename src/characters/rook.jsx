88ce374c0a9f0a9765ab49d824037c2ee103bf93
hobusu <the.isaac.adams@gmail.com>
2017-05-16 11:08:17 -0400

    Added 1st Ed. Rook as a variant

--- src/characters/rook.jsx
+++ src/characters/rook.jsx
@@ -32,11 +32,13 @@ export const rook = {
         innateAbilities: [
             {
                 name: 'Rock Armor',
-                text: 'If your normal attack is hit by the opponent\'s faster normal \
-                       attack or special attack, the opponent finishes his combo, then \
-                       if you were not knocked down, you may discard two cards of the \
-                       same suit as your attack. If you do, your attack is not interrupted \
-                       and you may finish your combo.',
+                text: <span>
+                    If your normal attack is hit by the opponent's faster normal
+                    attack or special attack, the opponent finishes their combo,
+                    then if you were not knocked down, you may discard two
+                    cards of the same suit as your attack. If you do, your attack is
+                    not interrupted and you may finish your combo.
+                </span>
             },
             {
                 name: 'Defense Mastery',
@@ -48,17 +50,21 @@ export const rook = {
                 rank: 3,
                 name: 'Entangling Vines',
                 timing: 'During Combat',
-                text: 'This block avoids block damage, knocks down attackers, and deals 5 \
-                       damage to them. When it does, next turn your attacks and throws are 3 \
-                       speed faster, to a minimum of speed 1.0. (This doesn\'t return this \
-                       card to your hand or draw from blocking.)',
+                text: <span>
+                    This block avoids block damage, knocks down attackers, and
+                    deals 5 damage to them. When it does, next turn your attacks
+                    and throws are 3 speed faster, to a minimum of speed 1.0.<br />
+                 <i>(This doesn't return to hand or draw from blocking.)</i>
+                </span>
             },
             {
                 rank: 5,
                 name: 'Stone Wall',
                 timing: 'During Combat',
-                text: 'This block reflects an attack\'s damage back to the attacker. (This \
-                       doesn\'t return this card to your hand or draw from blocking.)',
+                text: <span>
+                    This block reflects an attack's damage back to the attacker.<br />
+                 <i>(This doesn't return to hand or draw from blocking.)</i>
+                </span>
             },
             {
                 rank: 'K',
@@ -158,5 +164,48 @@ rook.variants = {
         throws: rook.throws.concat([
             {speed: 4.0, rank: 'D', name: 'Tanglevine', damage: 7, comboType: <CantCombo/>},
         ]),
+    }),
+    FirstEd: Object.assign({}, rook, {
+        summary: Object.assign({}, rook.summary, {
+            name: <span>1<sup>st</sup> Ed. {rook.summary.name}</span>,
+            fullName: <span>1<sup>st</sup> Edition {rook.summary.fullName}</span>,
+            innateAbilities: [
+                {
+                    name: 'Rock Armor',
+                    text: <span>
+                        If your normal attack is hit by the opponent's faster normal
+                        attack or special attack, the opponent finishes his combo,
+                        then if you were not knocked down, you may discard two
+                        cards of the same suit as your attack. If you do, your attack is
+                        not interrupted and you may finish your combo.
+                    </span>
+                },
+            ],
+            cardAbilities: [
+                {
+                    rank: 3,
+                    name: 'Entangling Vines',
+                    timing: null,
+                    text: <span>
+                        If you block an attack or joker with this card, take no
+                        block damage. You may discard a normal throw card from your
+                        hand to deal 8 damage to the opponent. Do not draw a card
+                        from blocking with Entangling Vines and don't return it to
+                        your hand unless it was countered.
+                    </span>
+                },
+                {
+                    rank: 5,
+                    name: 'Stone Wall',
+                    timing: null,
+                    text: <span>
+                        If you block an attack with this card, the attack does full
+                        damage to the attacker and no damage to you. Do not draw a
+                        card from blocking this way and don't return this card to your
+                        hand unless it was countered.
+                    </span>
+                },
+            ],
+        }),
     }),
 };
