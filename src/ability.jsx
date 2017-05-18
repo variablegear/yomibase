c38b9c6bf8adb430857bd071890011819bca05ef
Calen Pennington <cale@edx.org>
2017-05-18 12:01:17 -0400

    Remove empty timing tags (fixes #22)

--- src/ability.jsx
+++ src/ability.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 export function CardAbility(entry, multiline) {
     return [
-        <dt key={entry.rank + '-rank'}>{entry.rank} - {entry.name}{multiline ? <br /> : ' '}[{entry.timing}]</dt>,
+        <dt key={entry.rank + '-rank'}>{entry.rank} - {entry.name}
+            {multiline ? <br /> : ' '}
+            {entry.timing && <span>[{entry.timing}]</span>}
+        </dt>,
         <dd key={entry.rank + '-text'}>{entry.text}</dd>,
     ];
 }
