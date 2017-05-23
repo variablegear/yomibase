273bfa0044285574e690b3ecf2b0c7efb14aacec
Calen Pennington <cale@edx.org>
2017-05-23 10:25:08 -0400

    Add missing editions.jsx file

---
+++ src/editions.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const EX = <span>2<sup>nd</sup> Edition EX</span>;
+export const First = <span>1<sup>st</sup> Edition</span>;
+export const Second = <span>2<sup>nd</sup> Edition</span>;
