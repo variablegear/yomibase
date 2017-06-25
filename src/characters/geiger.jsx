b7b3108d987f2f892749d0779cbc16830e1d0225
hobusu <the.isaac.adams@gmail.com>
2017-06-25 10:22:52 -0400

    Added 1st Edition Geiger and improved formatting

--- src/characters/geiger.jsx
+++ src/characters/geiger.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {CantCombo, Ender, Linker, Starter, ComboDetails} from '../combo.jsx';
 import {mkNormal} from '../move.jsx';
-import {EX} from '../editions.jsx';
+import {EX, First} from '../editions.jsx';
 
 const normalAttack = mkNormal(0.6);
 
@@ -50,18 +50,22 @@ export const geiger = {
                 rank: 4,
                 name: 'Temporal Distortion',
                 timing: 'Draw Phase',
-                text: 'Fetch a Jack or Queen from your discard pile. Ongoing. Your Time \
-                       Spirals are zero combo point Linkers that do +1 damage each and \
-                       are immune to Rewind Time Jokers. Discard this card when you get \
-                       hit by an attack or throw.',
+                text: <span>
+                    Fetch a Jack or Queen from your discard pile.<br>
+                    <b>Ongoing.</b> Your Time Spirals are 0 combo point Linkers that
+                    do +1 damage each and are immune to Rewind Time Jokers.
+                    Discard this card when you get hit by an attack or throw.
+                </span>
             },
             {
                 rank: 8,
                 name: 'Research and Development',
                 timing: 'Draw Phase',
-                text: 'Look at the top X cards of your deck, put one in your hand, then \
-                       put the rest back on top of your deck in any order. X is the number \
-                       of Time Spiral cards in your discard pile.',
+                text: <span>
+                    Look at the top X cards of your deck, put one in your hand,
+                    then put the rest back on top of your deck in any order.<br>
+                    X is the number of Time Spiral cards in your discard pile.
+                </span>
             },
         ],
     },
@@ -111,6 +115,7 @@ geiger.variants = {
     EX: Object.assign({}, geiger, {
         summary: Object.assign({}, geiger.summary, {
             name: <span>EX {geiger.summary.name}</span>,
+            fullName: <span>EX {geiger.summary.fullName}</span>,
             edition: EX,
             innateAbilities: [
                 {
@@ -118,9 +123,9 @@ geiger.variants = {
                     text: <span>
                         If your opponent takes block damage from a Time
                         Spiral, they can't activate innate abilities from blocking,
-                        and you may throw them. (Play a throw card from your
+                        and you may throw them. <i>(Play a throw card from your
                         hand and continue your combo if you want. The opponent
-                        doesn't draw a card from blocking if you throw them.)
+                        doesn't draw a card from blocking if you throw them.)</i><br>
                         At the end of combat, you may fetch a Jack or Queen
                         from your discard pile.
                     </span>
@@ -148,5 +153,48 @@ geiger.variants = {
                 comboPts: 1, comboType: <Starter/>
             },
         ]),
+    }),
+    
+    FirstEd: Object.assign({}, geiger, {
+        summary: Object.assign({}, geiger.summary, {
+            name: <span>1<sup>st</sup> Ed. {geiger.summary.name}</span>,
+            edition: First,
+            innateAbilities: [
+                {
+                    name: 'Time Stop',
+                    text: <span>
+                        If your opponent takes block damage from a Time Spiral,
+                        you may throw him. <i>(Play a throw card from your hand and
+                        continue your combo if you want. The opponent doesn't
+                        draw a card from blocking if you throw him.)</i>
+                    </span>
+                },
+            ],
+            cardAbilities: [
+                {
+                    rank: 4,
+                    name: 'Temporal Distortion',
+                    timing: null,
+                    text: <span>
+                        After the draw phase, you may attach this card to your character card.
+                        If you do, return a Jack or Queen from your discard pile to your hand.
+                        While this card is attached, your Time Spirals cost zero combo points,
+                        are Linkers, deal +1 damage, and are immune to combo escape Jokers.
+                        Put this card in your discard pile when you get hit by an attack or
+                        throw. <i>(The Ace's Time Spiral Hurricane counts as a Time Spiral.)</i>
+                    </span>
+                },
+                {
+                    rank: 8,
+                    name: 'Research and Development',
+                    timing: null,
+                    text: <span>
+                        After the draw phase, you may discard this card to look at the top X cards
+                        of your deck, then put them back on top of your deck in any order. X is the
+                        number of Time Spiral cards (including Aces) in your discard pile. Draw a card.
+                    </span>
+                },
+            ],
+        }),
     }),
 };
