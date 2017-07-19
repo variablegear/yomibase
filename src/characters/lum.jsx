9ee996d58dd44d6fce7e32e6c89f5ef12212cff0
hobusu <the.isaac.adams@gmail.com>
2017-07-18 22:00:43 -0400

    Fixed a dumb typo
    Seriously, how did I get "1<st>" instead of 1<sup>st</sup>"?

--- src/characters/lum.jsx
+++ src/characters/lum.jsx
@@ -298,7 +298,7 @@ lum.variants = {
     }),
     FirstEd: Object.assign({}, lum, {
         summary: Object.assign({}, lum.summary, {
-            name: <span>1<st> Ed. {lum.summary.name}</span>,
+            name: <span>1<sup>st</sup> Ed. {lum.summary.name}</span>,
             innateAbilities: [
                 {
                     name: 'Roll the Dice',
